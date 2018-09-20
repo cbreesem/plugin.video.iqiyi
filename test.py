074eea1ec3342f298cc718956437d577ccfe2ce5
JeFF.Hou <325880010@qq.com>
2018-09-20 09:28:28 +0800

    还差获取视频地址功能

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# import xbmc, xbmcgui, xbmcplugin, xbmcaddon, urllib2, urllib, re, string, sys, os, gzip, StringIO
+# import xbmc, xbmcgui, xbmcplugin, xbmcaddon
 import urllib, urllib2, re, string, sys, os, gzip, StringIO
 from uuid import uuid4
 from random import random,randint
@@ -7,6 +7,7 @@ from math import floor
 import hashlib
 import simplejson
 import json
+
 
 # Plugin constants
 # __addonname__ = "爱奇艺视频"
@@ -48,7 +49,6 @@ def getHttpData(url,header=headers):
             httpdata = httpdata.decode(charset, 'ignore').encode('utf8', 'ignore')
     return httpdata
 
-
 def getParams():
     param = {}
     paramstring = sys.argv[2] if len(sys.argv) >= 3 else []
@@ -80,15 +80,17 @@ def getContentList(id,url='----------------iqiyi--.html'):
 
     # 获取筛选列表
     filters = re.compile('<div class="mod_sear_list(.+?)</div>', re.DOTALL).findall(html)
+
     for i in filters:
         option = re.compile('<h3>(.+?)</h3>', re.DOTALL).findall(i)
         if len(option) == 0 or '频道' in option[0]: continue
+        
         links = re.compile('<a href="/www/'+id+'/(.+?)</a>', re.DOTALL).findall(i)
-        links = [l.split('">') for l in links]
+        links = [l.replace('">','<>') for l in links]
 
-        # li = xbmcgui.ListItem('%s(选择)' % option[0][:-1])
-        # u = sys.argv[0]+"?mode=4&id="+urllib.quote_plus(id)+"&url="+urllib.quote_plus(json.dumps(links))
-        # xbmcplugin.addDirectoryItem(int(sys.argv[1]), u, li, True)
+        li = xbmcgui.ListItem('%s(选择)' % option[0][:-3])
+        u = sys.argv[0]+"?mode=4&id="+urllib.quote_plus(id)+"&uri="+urllib.quote_plus(option[0][:-3]+'<||>'+'<|>'.join(links))
+        xbmcplugin.addDirectoryItem(int(sys.argv[1]), u, li, True)
 
     # 获取页码列表
     pages = re.compile('<a data-key="up|down" class="a1"|data-search-page="item"(.+?)</a>', re.DOTALL).findall(html)
@@ -97,6 +99,7 @@ def getContentList(id,url='----------------iqiyi--.html'):
 
     # 获取内容列表
     content = re.compile('<ul class="site-piclist site-piclist-180236 site-piclist-auto">(.+?)</ul>', re.DOTALL).findall(html)
+    # xbmc.log(str(content))
     content = re.compile('<li>(.+?)</li>', re.DOTALL).findall(content[0])
     for i in content:
         pid = re.compile('data-qidanadd-albumid="(\d+)"').search(i).group(1)
@@ -118,44 +121,134 @@ def getContentList(id,url='----------------iqiyi--.html'):
 
         mode,isdir = ('2',True) if '更新' in filmLength or '集全' in filmLength else ('3',False)
 
-        # print uri,name
+        li = xbmcgui.ListItem(name, iconImage = '', thumbnailImage = thumb)
+        u = sys.argv[0]+"?mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&thumb="+urllib.quote_plus(thumb)+"&uri="+urllib.quote_plus(uri)
+        li.setInfo(type = "Video", infoLabels = {"Title":name, "Cast":cast, "Rating":rating})
+        xbmcplugin.addDirectoryItem(int(sys.argv[1]), u, li, isdir)
 
-        # li = xbmcgui.ListItem(name, iconImage = '', thumbnailImage = thumb)
-        # u = sys.argv[0]+"?mode="+str(mode)+"&id="+urllib.quote_plus(pid)+"&thumb="+urllib.quote_plus(thumb)+"&uri="+urllib.quote_plus(uri)
-        # li.setInfo(type = "Video", infoLabels = {"Title":name, "Cast":cast, "Rating":rating})
-        # xbmcplugin.addDirectoryItem(int(sys.argv[1]), u, li, isdir)
-
-    # xbmcplugin.endOfDirectory(int(sys.argv[1]))
+    xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 def getSeriesList(uri):
-    html = getHttpData(uri)
-    series = re.compile('<ul class="site-piclist(.+?)</ul>', re.DOTALL).findall(html)[0]
-    series = re.compile('<li data-albumlist-elem="playItem">(.+?)</li>', re.DOTALL).findall(series)
+    html = getHttpData(urllib.unquote(uri))
+    series = re.compile('<ul class="site-piclist(.+?)</ul>', re.DOTALL).findall(html)
+    series = re.compile('<li data-albumlist-elem="playItem">(.+?)</li>', re.DOTALL).findall(series[0])
 
     for i in series:
         thumb = re.compile('src\s*=\s*"(.+?)"').findall(i)[0]
-        name,number = re.compile('<a [^>]+>([^<]*)</a>').findall(i)
+        number,name = re.compile('<a [^>]+>([^<]*)</a>').findall(i)
 
         uri  = re.compile('href="([^"]*)"').search(i).group(1)
         filmLength = re.compile('<span class="mod-listTitle_right">(\d+?):(\d+?)</span>').findall(i)
-        print filmLength,name.strip(),number.strip()
+        # print filmLength,name.strip(),number.strip(),uri
 
-    #     li = xbmcgui.ListItem('%s-%s' % (name.strip(),number.strip()), iconImage = '', thumbnailImage = thumb)
-    #     li.setInfo(type = "Video", infoLabels = {"Title":p_name, "Director":p_director, "Cast":p_cast, "Plot":p_plot, "Year":p_year})
-    #     u = sys.argv[0] + "?mode=3&name=" + urllib.quote_plus(p_name) + "&id=" + urllib.quote_plus(p_id)+ "&thumb=" + urllib.quote_plus(thumb)
-    #     xbmcplugin.addDirectoryItem(int(sys.argv[1]), u, li, False, totalItems)
-    # xbmcplugin.endOfDirectory(int(sys.argv[1]))
+        li = xbmcgui.ListItem('%s-%s %s' % (number.strip(),name.strip(),':'.join(filmLength[0])), iconImage = '', thumbnailImage = thumb)
+        li.setInfo(type = "Video", infoLabels = {"Title":name})
+        u = sys.argv[0] + "?mode=3&name=" + urllib.quote_plus(number.strip()+'-'+name.strip()) + "&thumb=" + urllib.quote_plus(thumb) + "&uri=" + urllib.quote_plus(uri)
+        xbmcplugin.addDirectoryItem(int(sys.argv[1]), u, li, False)
+    xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
+def performChanges(id,uri):
+    option,url = urllib.unquote(uri).split('<||>')
+    dialog = xbmcgui.Dialog()
 
+    if len(url) == 0: return
+
+    names = [x.split('<>')[1] for x in url.split('<|>') if len(x.split('<>')) > 1]
+    urls = [x.split('<>')[0] for x in url.split('<|>') if len(x.split('<>')) > 1]
+
+    sel = dialog.select(option, names)
+
+    if sel != -1: getContentList(id,urls[sel])
+
+def playVideo(name,uri):
+    html = getHttpData(urllib.unquote(uri))
+    print(html)
+
+    return 
+    id = id.split(',')
+    if len(id) == 1:
+        try:
+            if ("http:" in id[0]):
+                link = GetHttpData(id[0])
+                tvId = re.compile('data-player-tvid="(.+?)"', re.DOTALL).findall(link)[0]
+                videoId = re.compile('data-player-videoid="(.+?)"', re.DOTALL).findall(link)[0]
+            else:
+                url = 'http://cache.video.qiyi.com/avlist/%s/' % (id[0])
+                link = GetHttpData(url)
+                data = link[link.find('=')+1:]
+                json_response = simplejson.loads(data)
+                tvId = str(json_response['data']['vlist'][0]['id'])
+                videoId = json_response['data']['vlist'][0]['vid'].encode('utf-8')
+        except:
+            dialog = xbmcgui.Dialog()
+            ok = dialog.ok(__addonname__, '未能获取视频地址')
+            return
+    else:
+         tvId = id[0]
+         videoId = id[1]
+
+    gen_uid = uuid4().hex
+    info = getVMS(tvId, videoId, gen_uid)
+    if info["code"] != "A000000":
+        dialog = xbmcgui.Dialog()
+        ok = dialog.ok(__addonname__, '密钥过期，需升级插件')
+        return
+
+    vs = info["data"]["vp"]["tkl"][0]["vs"]
+    #print [(x['bid'],x['scrsz']) for x in vs]
+    sel = selResolution([x['bid'] for x in vs])
+    if sel == -1:
+        return
+
+    video_links = vs[sel]["fs"] #now in i["flvs"] not in i["fs"]
+    if not vs[sel]["fs"][0]["l"].startswith("/"):
+        tmp = getVrsEncodeCode(vs[sel]["fs"][0]["l"])
+        if tmp.endswith('mp4'):
+             video_links = vs[sel]["flvs"]
+
+    # New method to avoid url expired
+    qyplayer.play(name, thumb, info["data"]["vp"]["du"], video_links, gen_uid)
+    while qyplayer.is_active:
+        xbmc.sleep(100)
+
+    # Old method
+    #urls = []
+    #size = 0
+    #for i in video_links:
+    #    vlink = i["l"]
+    #    if not vlink.startswith("/"):
+    #        #vlink is encode
+    #        vlink=getVrsEncodeCode(vlink)
+    #    key=getDispathKey(vlink.split("/")[-1].split(".")[0])
+    #    size+=i["b"]
+    #    baseurl=info["data"]["vp"]["du"].split("/")
+    #    baseurl.insert(-1,key)
+    #    url="/".join(baseurl)+vlink+'?su='+gen_uid+'&qyid='+uuid4().hex+'&client=&z=&bt=&ct=&tn='+str(randint(10000,20000))
+    #    urls.append(simplejson.loads(GetHttpData(url))["l"])
+
+    #stackurl = 'stack://' + ' , '.join(urls)
+    #listitem = xbmcgui.ListItem(name,thumbnailImage=thumb)
+    #listitem.setInfo(type="Video",infoLabels={"Title":name})
+    #xbmc.Player().play(stackurl, listitem)
 # params = getParams()
 # mode = params.get('mode')
 # name = params.get('name')
 # id = params.get('id')
 
-# if mode == None: getRootList()
-# elif mode == '1': getContentList(id, params.get('uri'))
-# elif mode == '2': getSeriesList(params.get('uri'))
-
-getContentList('1')
-getSeriesList('http://www.iqiyi.com/a_19rrj5x3zh.html#vfrm=2-4-0-1')
-getSeriesList('http://www.iqiyi.com/a_19rrh5cl15.html#vfrm=2-4-0-1')
+# if mode == None: 
+#     getRootList()
+# elif mode == '1': 
+#     if params.get('uri') is None:
+#         getContentList(id)
+#     else:
+#         getContentList(id, params.get('uri'))
+# elif mode == '2':
+#     getSeriesList(params.get('uri'))
+# elif mode == '3':
+#     playVideo(name,id,thumb)
+# elif mode == '4':
+#     performChanges(id,params.get('uri'))
+playVideo('第40集-周志超乔装司机','http://www.iqiyi.com/v_19rr05rei8.html')
+# getContentList('1')
+# getSeriesList('http://www.iqiyi.com/a_19rrj5x3zh.html#vfrm=2-4-0-1')
+# getSeriesList('http://www.iqiyi.com/a_19rrh5cl15.html#vfrm=2-4-0-1')
